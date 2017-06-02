33421b64bad22e9a815a57206c01ab043a29697e
Patrick <pmoore4181@gmail.com>
2017-06-02 15:59:18 -0500

    fully operational. will stylize

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,32 +33,79 @@ var animals = ["doggo", "cat", "squirrel", "seal"];
       // This line grabs the input from the textbox
       var newAnimal = $("#animal-input").val().trim();
 
-      // The movie from the textbox is then added to our array
+      // The animal from the textbox is then added to our array
       animals.push(newAnimal);
 
-      // Calling renderButtons which handles the processing of our movie array
+      if (newAnimal == "") {
+        alert("Type in an animal, please");
+      }
+
+      // Calling renderButtons which handles the processing of our animals array
       renderButtons();
     });
 
     renderButtons();
 
-    // Retrieve the JSON data for each button
+    // Retrieve the JSON data for each button and put it on page
     function retrieveGifInfo() {
 
       var animalButton = $(this).attr("data-name");
-      var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=" + APIkey + "&q=" + animalButton  + "&limit=10";
+      var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIkey + "&q=" + animalButton  + "&limit=10";
 
       $.ajax({
         url: queryURL,
         method: "GET"
       }).done(function(response) {
         $("#animalGifs").empty();
+        console.log(response);
         for (var i = 0; i<=9; i++){
-            newGifs = response.data[i].embed_url;
-            $("#animalGifs").append("<iframe src='" + newGifs + "'</iframe>");
+            var stillGif = response.data[i].images.original_still.url;
+            var movingGif = response.data[i].images.original.url;
+            var gifRating = response.data[i].rating;
+            $("#animalGifs")
+                .append("<div class='gifRating'>Rating: " + gifRating + "</div>")
+                .append("<img class='gif' data-state='still' src='" + stillGif + "' data-still='" + stillGif + "' data-animate='" + movingGif + "'>");
         }
+
+        $(".gif").on("click", function() {
+          // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+          var state = $(this).attr("data-state");
+          // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+          // Then, set the image's data-state to animate
+          // Else set src to the data-still value
+          if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+        });
+
       });
+
     }
+    
+
+
+
+//  ===================  Use this to make it work! ====================================
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
     $(document).on("click", ".animal", retrieveGifInfo);
 
